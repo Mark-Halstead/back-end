3e40beb1e8a2dc26f2f12c7a3e67bf132d6a9d46
Mark Halstead <103516032+Mark-Halstead@users.noreply.github.com>
2023-01-17 11:21:00 +0000

    Add filter id

--- server/app.js
+++ server/app.js
@@ -9,12 +9,31 @@ const app = express() // Make a very basic server using express
 
 app.get("/", (req,res) => {
     res.json({
-        "message": "Hello World!"
+        "message": "Welcome to the goat api!"
     })
 })
 
 app.get("/goats", (req, res) => {
     res.status(503).json(goats)
+})
+
+// Getting individual goats
+app.get("/goats/:id", (req,res) => {
+
+    const id = req.params["id"]
+
+    // Filter the goat list for the relevant goat
+    const goat = goats.filter(g => g["id"] == id)[0]
+
+    if (goat) {
+        res.json(goat)
+    } else {
+        //Send a status of 404 with a message
+        res.status(404).json({
+            error: "No such goat!"
+        })
+    }
+    res.json(goat)
 })
 
 module.exports = app //Make server available to other files
