0fa4643c7226c442abfedef66c3b7c95fca1ceb6
Mark Halstead <103516032+Mark-Halstead@users.noreply.github.com>
2023-01-17 10:52:19 +0000

    Setup data store

--- server/app.js
+++ server/app.js
@@ -1,13 +1,20 @@
 // Describes the server/API to other files
 
 const express = require("express") //Access to the express library
+const { goats, nextId } = require("./goats")
 
 const app = express() // Make a very basic server using express
 
 // Tell the app what kinds of requests to listen for (and how to handle them)
 
 app.get("/", (req,res) => {
-    res.send("Hello World!")
+    res.json({
+        "message": "Hello World!"
+    })
+})
+
+app.get("/goats", (req, res) => {
+    res.status(503).json(goats)
 })
 
 module.exports = app //Make server available to other files
---
+++ server/goats.js
@@ -0,0 +1,22 @@
+const goats = 
+    [
+        { "id": 1,
+        "name": "Billy",
+        "age" : 6,
+        "sex": "M",
+        "favoriteColor": "blue" },
+        { "id": 2,
+        "name": "Beehroncia",
+        "age" : 100,
+        "sex": "F",
+        "favoriteColor": "blue" },
+        { "id": 3,
+        "name": "Ezekiel",
+        "age" : 15,
+        "sex": "M",
+        "favoriteColor": "blue" }
+    ]
+
+let nextId = 4
+
+module.exports = { goats, nextId }
