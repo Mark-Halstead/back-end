21c66143a44e4ae99d42ce71371d4ca8f8e052a1
Mark Halstead <103516032+Mark-Halstead@users.noreply.github.com>
2023-01-17 15:37:41 +0000

    Adding deletion functionality

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@
 
 const express = require("express") //Access to the express library
 const cors = require("cors")
-const { goats, nextId } = require("./goats")
+let { goats, nextId } = require("./goats")
 
 
 const app = express() // Make a very basic server using express
@@ -14,8 +14,9 @@ const logger = require("./logger")
 // req -> [cors (add header to response)] -> [API] -> response
 // req -> [auth (checks the req headers for a key)] -> [API] -> response
 
-app.use(cors())
-app.use(logger)
+app.use(express.json()) // Layer to read body of posts
+app.use(cors()) // Layer to add CORS headers
+app.use(logger) // Layer to log access
 
 //Endpoints
 
@@ -37,6 +38,24 @@ app.get("/goats", (req, res) => {
     }
 })
 
+app.post("/goats", (req, res) => {
+
+    // Extract the information
+    const newGoat = req.body
+    
+    // Add the id to the goats data
+    newGoat["id"] = nextId
+
+    // Increase the nextId for next time
+    nextId += 1
+
+    // Add the goat to the goat list
+    goats.push(newGoat)
+
+    // Report our success
+    res.status(201).json(newGoat)
+})
+
 // Getting individual goats
 app.get("/goats/:id", (req,res) => {
 
@@ -54,6 +73,27 @@ app.get("/goats/:id", (req,res) => {
         })
     }
     res.json(goat)
+})
+
+app.delete("/goats/:id", (req, res) => {
+
+    // Pull out the id from the URL
+    const id = req.params["id"]
+
+    // Check if that goat is real
+    const exists = goats.filter(g => g["id"] == id).length == 1
+
+    // If it is,
+    if (exists) {
+        // Delete goat / create a new version of goats that dosen't contain it
+        goats = goats.filter(g => g["id"] != id)
+        // Return a relevant status and message
+        res.sendStatus(204)
+    } else {
+        res.status(404).json({
+            error: "No such goat!"
+        })
+    }
 })
 
 module.exports = app //Make server available to other files
